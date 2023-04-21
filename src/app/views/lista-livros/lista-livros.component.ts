780994e383bc417b50e6835911f2ea2210b25791
joneilson <joneilson84@gmail.com>
2023-04-20 21:51:53 -0300

    SERVICE: adicionando serviço de busca

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnInit {
 
   listaLivros: [];
 
-  constructor() { }
 
+  constructor(private livroService: LivroService) { }
+
+  ngOnInit(): void {
+
+  }
 }
 
 
